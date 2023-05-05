4f08adb9f7ea51fd0ce4e8d90f37bfc5a20af65c
DilisTech <info.dilistech@gmail.com>
2023-05-05 19:37:46 +0100

    Add project files.

---
+++ Controllers/SubscribersController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneBook.Data;
+using PhoneBook.Models;
+
+
+namespace PhoneBook.Controllers
+{
+    public class SubscribersController : Controller
+    {
+        private readonly AppDbContext _db;
+        public SubscribersController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Subscriber> objList = _db.Subscribers;
+            return View(objList);
+        }
+
+
+        public IActionResult Create()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Subscriber obj)
+        {
+            _db.Subscribers.Add(obj);   
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Subscribers.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Subscriber obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Subscribers.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Subscribers.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Subscribers.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.Subscribers.Remove(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneBook.Models;
+
+namespace PhoneBook.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Subscriber> Subscribers { get; set; }
+    }
+}
